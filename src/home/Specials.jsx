828538e004cb503e755c9c6c1a13a3b239e1f38f
Patricia <patricianjuguna084@gmail.com>
2026-01-25 17:27:33 +0300

    feat: setup boilerplate hero specials content

--- src/home/Specials.jsx
+++ src/home/Specials.jsx
@@ -1,5 +1,21 @@
+import Button from "../components/Button"
+import Special from "../components/Special"
+import GreekSalad from "../assets/greek salad.jpg"
+import Bruchetta from "../assets/bruchetta.svg"
+import LemonDessert from "../assets/lemon dessert.jpg"
+
 export default function Specials(){
     return(
-        <h1>This week's specials</h1>
+        <div>
+            <div>
+                <h1>This Week's Specials!</h1>
+                <Button label={'Order Online'}/>
+            </div>
+            <div>
+                <Special foodItem={GreekSalad} description={'Fresh Greek salad with crisp lettuce, juicy tomatoes, creamy feta, olives, and tangy dressing.'} title={'Greek Salad'} price={'$ 12.99'}/>
+                <Special foodItem={Bruchetta} description={'Bruschetta: Italian appetizer featuring toasted bread topped with fresh tomatoes, garlic, basil, and olive oil.'} title={'Bruchetta'} price={'$ 8.99'}/>
+                <Special foodItem={LemonDessert} description={'A zesty lemon dessert bursting with citrus flavor, balanced sweetness, and a refreshing, tangy finish.'} title={'Lemon Dessert'} price={'$ 5.00'}/>
+            </div>
+        </div>
     )
 }

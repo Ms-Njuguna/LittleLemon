c05eca81072abc726477a3d2d8a38c117c55a408
Patricia <patricianjuguna084@gmail.com>
2026-01-25 14:47:12 +0300

    feat: setup boilerplate hero section content

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button({label}){
+    return (
+        <button className="border px-3 py-2 rounded-2xl">{label}</button>
+    )
+}
--- src/home/Hero.jsx
+++ src/home/Hero.jsx
@@ -1,5 +1,18 @@
+import Button from "../components/Button"
+import Food from "../assets/restauranfood.jpg"
+
 export default function Hero(){
     return (
-        <h1>Hero section</h1>
+        <div>
+            <div>
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+                <p>Little Lemon is a charming neighbourhood<br></br> bistro that serves simple food and<br></br> classic cocktails in a lively but casual environment.<br></br> The restaurant features a locally-sourced<br></br> menu with daily specials.</p>
+                <Button label={'Reserve A Table'}/>
+            </div>
+            <div>
+                <img src={Food} alt="Restaurant food"/>
+            </div>
+        </div>
     )
 }

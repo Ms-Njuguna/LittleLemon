d8ed736d23228826ecaa67525d6c509ca1e5ed14
Patricia <patricianjuguna084@gmail.com>
2026-03-11 11:46:37 +0300

    feat(services): add reservation constants and initial form state

---
+++ src/services/reservationData.js
@@ -0,0 +1,37 @@
+export const DINER_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const OCCASION_OPTIONS = [
+  "Birthday",
+  "Anniversary",
+  "Date Night",
+  "Business Dinner",
+  "Family Gathering",
+  "Other",
+];
+
+export const TIME_OPTIONS = [
+  "12:00 pm",
+  "1:00 pm",
+  "2:00 pm",
+  "3:00 pm",
+  "4:00 pm",
+  "5:00 pm",
+  "6:00 pm",
+  "7:00 pm",
+  "8:00 pm",
+  "9:00 pm",
+];
+
+export const INITIAL_RESERVATION_FORM = {
+  seating: "Outdoor seating",
+  date: null,
+  diners: "",
+  occasion: "",
+  time: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  requests: "",
+  agree: false,
+};
